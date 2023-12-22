944e9155edbe03e3c71bb2e4d6d070e5f23d5199
MobarakHsn <mobarak@appscode.com>
2023-12-22 16:10:15 +0600

    Intialized
    Signed-off-by: MobarakHsn <mobarak@appscode.com>

---
+++ book/book_suite_test.go
@@ -0,0 +1,13 @@
+package book_test
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+func TestBook(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Book Suite")
+}
---
+++ book/book_test.go
@@ -0,0 +1,12 @@
+package book_test
+
+import (
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+
+	"github.com/MobarakHsn/Ginkgo/book"
+)
+
+var _ = Describe("Book", func() {
+
+})
