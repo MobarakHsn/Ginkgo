c961d20deac8fde2ec75bc4caebfe8cac1c13782
MobarakHsn <mobarak@appscode.com>
2023-12-27 11:54:20 +0600

    Basic testing
    Signed-off-by: MobarakHsn <mobarak@appscode.com>

--- book/book_test.go
+++ book/book_test.go
@@ -1,12 +1,42 @@
 package book_test
 
 import (
+	"github.com/MobarakHsn/Ginkgo/book"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
-
-	"github.com/MobarakHsn/Ginkgo/book"
+	"log"
 )
 
-var _ = Describe("Book", func() {
+var _ = Describe("Book.isCheap()", func() {
+	Context("When is the book is cheap", func() {
+		It("returns true", func() {
+			book := book.Book{Price: 499}
+			response := book.IsCheap()
+			Expect(response).To(BeTrue())
+		})
+	})
 
+	Context("When is the book is not cheap", func() {
+		AfterEach(func() {
+			log.Println("Okay, not cheap")
+		})
+		BeforeEach(func() {
+			log.Println("Not cheap")
+		})
+
+		It("returns false", func() {
+			book := book.Book{Price: 503}
+			response := book.IsCheap()
+			Expect(response).To(BeFalse())
+		})
+
+	})
+
+	DescribeTable("IsCheap table test",
+		func(price int, expectedResponse bool) {
+			b := book.Book{Price: price}
+			Expect(b.IsCheap()).To(Equal(expectedResponse))
+		},
+		Entry("When a book is cheap", 500, true),
+		Entry("When a book is not cheap", 555, false))
 })
--- book/main.go
+++ book/main.go
@@ -1,1 +1,13 @@
 package book
+
+type Book struct {
+	name  string
+	Price int
+}
+
+func (b *Book) IsCheap() bool {
+	if b.Price <= 500 {
+		return true
+	}
+	return false
+}
